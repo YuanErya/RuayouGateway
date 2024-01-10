19f52256d4b7f17e3d142d0dc5d94364fe902755
YuanEr <syuanerya@163.com>
2024-01-10 19:52:59 +0800

    create: 创建项目

---
+++ RuayouGateway-Core/src/main/java/com/ruayou/Application.java
@@ -0,0 +1,13 @@
+package com.ruayou;
+
+/**
+ * @Author：ruayou
+ * @Date：2024/1/2 18:34
+ * @Filename：Application
+ * 程序入口
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
